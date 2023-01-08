30ed18dbb7c71bd4b23f2abffca7c886bde23779
Paul Botros <pbotros@ucsd.edu>
2023-01-07 19:04:51 -0800

    Fixing drivers/mtd/nand/raw/arasan-nand-controller.c

--- drivers/mtd/nand/raw/arasan-nand-controller.c
+++ drivers/mtd/nand/raw/arasan-nand-controller.c
@@ -958,8 +958,6 @@ static int anfc_setup_interface(struct nand_chip *chip, int target,
 		if (IS_ERR(sdr))
 			return PTR_ERR(sdr);
 	}
-	const struct nand_sdr_timings *sdr;
-	const struct nand_nvddr_timings *nvddr;
 
 	if (nand_interface_is_nvddr(conf)) {
 		nvddr = nand_get_nvddr_timings(conf);
@@ -977,10 +975,6 @@ static int anfc_setup_interface(struct nand_chip *chip, int target,
 	if (nand_interface_is_sdr(conf))
 		anand->timings = DIFACE_SDR |
 				 DIFACE_SDR_MODE(conf->timings.mode);
-	else
-		anand->timings = DIFACE_NVDDR |
-				 DIFACE_DDR_MODE(conf->timings.mode);
-
 	else
 		anand->timings = DIFACE_NVDDR |
 				 DIFACE_DDR_MODE(conf->timings.mode);
