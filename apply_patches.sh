4b9853e4e7c2aca275a00f9cd253f37aaf093ad6
Paul Botros <pbotros@ucsd.edu>
2023-01-09 08:29:43 -0800

    openwrt_patches/hack-5.10/205-kconfig-exit.patch

--- apply_patches.sh
+++ apply_patches.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 found=false;
-for i in `ls openwrt_patches/pending-5.10`
+for i in `ls openwrt_patches/hack-5.10`
 do
-	if [ "$found" = false ]; then
-		if [[ "$i" =~ ^483-mtd-spi-nor-add-gd25q512.patch.* ]]; then
-			found=true
-		fi
-		continue
-	fi
+	# if [ "$found" = false ]; then
+	# 	if [[ "$i" =~ ^483-mtd-spi-nor-add-gd25q512.patch.* ]]; then
+	# 		found=true
+	# 	fi
+	# 	continue
+	# fi
 	echo $i
-	git am openwrt_patches/pending-5.10/$i;
+	git am openwrt_patches/hack-5.10/$i;
 	retval=$?;
 	if [ $retval -eq 128 ]; then
-		git apply openwrt_patches/pending-5.10/$i || break
+		git apply openwrt_patches/hack-5.10/$i || break
 		git add . || break
-		git commit -m "openwrt_patches/pending-5.10/$i" || break
+		git commit -m "openwrt_patches/hack-5.10/$i" || break
 		continue
 	elif [ $retval -eq 0 ]; then
 		continue
