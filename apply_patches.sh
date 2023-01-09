2571f182ea3a8284ab94c4a00b89c82ed59de589
Paul Botros <pbotros@ucsd.edu>
2023-01-09 08:19:00 -0800

    openwrt_patches/pending-5.10/400-mtd-mtdsplit-support.patch

--- apply_patches.sh
+++ apply_patches.sh
@@ -2,12 +2,12 @@
 found=false;
 for i in `ls openwrt_patches/pending-5.10`
 do
-	# if [ "$found" = false ]; then
-	# 	if [[ "$i" =~ ^801-v6.1-0001-nvmem-add-driver-hand.* ]]; then
-	# 		found=true
-	# 	fi
-	# 	continue
-	# fi
+	if [ "$found" = false ]; then
+		if [[ "$i" =~ ^101-Use-stddefs.h.* ]]; then
+			found=true
+		fi
+		continue
+	fi
 	echo $i
 	git am openwrt_patches/pending-5.10/$i || break
 done
